8da11d99ebf7edfd651ffd6c5cd2a0384f6787a9
Pysched <murphyjdarren@gmail.com>
2020-05-28 00:43:52 +0000

    updated app.py with base.html route

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-import json
-from datetime import datetime
+from flask import Flask, render_template, redirect, request, url_for, 
 from flask_pymongo import PyMongo
-from flask import Flask, render_template, request, flash, redirect, session, url_for
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'bookshelf'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-46ezx.mongodb.net/bookshelf?retryWrites=true&w=majority'
 
 
 
@@ -16,13 +17,12 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
 
 
 
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get("IP", "0.0.0.0"),
-            port=int(os.environ.get("PORT", "5000")),
-            debug=True)
+    app.secret_key = 'mysecret'
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get("PORT")), debug=True)
